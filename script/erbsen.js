64486c98a8a957ad7bc34b0b0a3cd1012f0d42d5
Tobias Lahmann <tobias.lahmann@uni-ulm.de>
2016-10-01 20:36:30 +0200

    Better distinction to true false

--- script/erbsen.js
+++ script/erbsen.js
@@ -1,15 +1,15 @@
 const solution = {
-    s1:"drag-bf47",
-    s2:"drag-d440",
-    s3:"drag-a6aa",
-    s4:"drag-d8ee",
-    s5:"drag-f42d",
-    s6:"drag-eacf",
-    s7:"drag-c864",
-    s8:"drag-e578",
-    s9:"drag-a961",
-    s10:"drag-ac39",
-    s11:"drag-aa44"
+    "dd_1":"drag-bf47",
+    "dd_2":"drag-d440",
+    "dd_3":"drag-a6aa",
+    "dd_4":"drag-d8ee",
+    "dd_5":"drag-f42d",
+    "dd_6":"drag-eacf",
+    "dd_7":"drag-c864",
+    "dd_8":"drag-e578",
+    "dd_9":"drag-a961",
+    "dd_10":"drag-ac39",
+    "dd_11":"drag-aa44"
 } // dont cheat!
 
 function allowDrop(ev) {
@@ -24,12 +24,15 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
-    check(data);
+    check(ev.target.id, data);
 }
 
 
-function check(dropID) {
+function check(targetID, dropID) {
     var d = document.getElementById(dropID);
-    d.className += " correct";
-    d.className += " wrong";
+    if (solution[targetID] == dropID) {
+        d.className += " correct";
+    } else {
+        d.className += " wrong";
+    }
 }
